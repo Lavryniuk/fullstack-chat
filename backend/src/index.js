6698765f5d52ae1bde0c827fcc626336ce0334ee
Artur <exalt.webdev.97@gmail.com>
2025-06-13 03:18:05 +0200

    done with profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   corse({
